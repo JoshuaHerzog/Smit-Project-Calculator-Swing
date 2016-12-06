ea32d6601ba76193b725bda6db60f2a5213d2c47
bbsuser <bbsuser@10.5.101.9>
2016-12-06 10:44:00 +0100

    + Added MVC Class Layout + Added View Function Stubs

---
+++ Taschenrechner/src/View/View.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.*;
+/**
+ *
+ * @author bbsuser
+ */
+public class View 
+{
+    
+    
+    public View()
+    {
+        GridLayout gridLayout = new GridLayout(3, 3);
+        
+        InitForm();
+    }
+    
+    private void InitForm()
+    {
+        
+    }
+    
+    public void ResetEingabe()
+    {
+        
+    }
+    
+    public void DeleteLastCharacter()
+    {
+        
+    }
+}
---
+++ Taschenrechner/src/taschenrechner/Taschenrechner.java
@@ -0,0 +1,24 @@
+package taschenrechner;
+
+import Controller.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bbsuser
+ */
+public class Taschenrechner {
+    static Controller fController;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        fController = new Controller();
+    }
+    
+}
