94ceca9743b48990efd8aa555b13a4d41495ba80
bbsuser <bbsuser@E201-08>
2016-12-13 10:35:22 +0100

    Anfängliche Class zur Berechnung, durchläuft den String und vergleicht chars

--- Taschenrechner/src/Model/Model.java
+++ Taschenrechner/src/Model/Model.java
@@ -7,14 +7,32 @@ package Model;
 
 /**
  *
- * @author Joshua Herzog
+ * @author Jan M. Groß, Joshua Herzog, Hendrik Neumannm
  */
 public class Model 
 {
-   public double Berechne(String fstr)
-   {
-     return 0;  
-   }
+    public double Berechne(String astr)
+    {
+       double ergebnis;
+       String zwischen_string_zahlen = "";
+       String zwischen_string_rechenzeichen = "";
+       int counter_rechenzeichen = 0;
+       ergebnis = 2.2;
+        // Durchlaufe den String Zeichen fuer Zeichen
+        for (int i = 0; i < astr.length(); i++){
+            char zeichen = astr.charAt(i);
+            //verarbeite Zeichen
+            if (zeichen == ',' || zeichen >= '0' && zeichen <= '9'){
+                zwischen_string_zahlen += zeichen;
+            }else{
+                zwischen_string_rechenzeichen += zeichen;
+                counter_rechenzeichen++;
+            }
+            
+        }
+
+        return ergebnis;
+    }
    
    public void ClearAll()
    {
