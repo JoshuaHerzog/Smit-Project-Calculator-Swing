d04d5720ed6a65b9c1be6e2ca4617848a8bf5e47
Max <Max@Max-PC>
2016-12-18 19:56:03 +0100

    + added Parenthesis Open/Closed o changed Button Layout

--- Taschenrechner/src/View/View.java
+++ Taschenrechner/src/View/View.java
@@ -24,7 +24,7 @@ public class View extends JFrame
     private JButton btnBack = new JButton("Back");*/
         GridLayout glCalc = new GridLayout(3, 1);
         GridLayout glTextField = new GridLayout(2, 1);
-        GridLayout glMidButtons = new GridLayout(2, 5);
+        GridLayout glMidButtons = new GridLayout(4, 3);
         GridLayout glBotButtons = new GridLayout(4, 3);
         
         JPanel pnlTop = new JPanel(glTextField);
@@ -55,6 +55,8 @@ public class View extends JFrame
         JButton buttonBackspace = new JButton();
         JButton buttonClearAll = new JButton();
         JButton buttonEquals = new JButton();
+        JButton buttonParenthesisOpen = new JButton();
+        JButton buttonParenthesisClosed = new JButton();
     
     public View()
     {
@@ -85,15 +87,17 @@ public class View extends JFrame
     
     private void PlaceButtonsOnPanels()
     {
-        pnlMidButtons.add(buttonCosinus);
-        pnlMidButtons.add(buttonSinus);
-        pnlMidButtons.add(buttonClearAll);
-        pnlMidButtons.add(buttonMultiply);
-        pnlMidButtons.add(buttonDivide);
         pnlMidButtons.add(buttonAdd);
+        pnlMidButtons.add(buttonSinus);
+        pnlMidButtons.add(buttonParenthesisOpen);
         pnlMidButtons.add(buttonSubtract);
-        pnlMidButtons.add(buttonBackspace);
+        pnlMidButtons.add(buttonCosinus);
+        pnlMidButtons.add(buttonParenthesisClosed);
+        pnlMidButtons.add(buttonMultiply);
         pnlMidButtons.add(buttonTangens);
+        pnlMidButtons.add(buttonBackspace);
+        pnlMidButtons.add(buttonDivide);
+        pnlMidButtons.add(buttonClearAll);
         pnlMidButtons.add(buttonEquals);
         pnlBotButtons.add(button7);
         pnlBotButtons.add(button8);
@@ -112,49 +116,29 @@ public class View extends JFrame
     private void InitializeButtons()
     {
         button0.setText("0");
-        button0.setSize(40, 40);    
         button1.setText("1");
-        button1.setSize(40, 40);    
         button2.setText("2");
-        button2.setSize(40, 40);    
         button3.setText("3");
-        button3.setSize(40, 40);    
         button4.setText("4");
-        button4.setSize(40, 40);    
         button5.setText("5");
-        button5.setSize(40, 40);    
         button6.setText("6");
-        button6.setSize(40, 40);    
         button7.setText("7");
-        button7.setSize(40, 40);    
         button8.setText("8");
-        button8.setSize(40, 40);    
         button9.setText("9");
-        button9.setSize(40, 40);    
-        buttonAdd.setText("+");
-        buttonAdd.setSize(40, 40);    
+        buttonAdd.setText("+");  
         buttonSubtract.setText("-");
-        buttonSubtract.setSize(40, 40);    
         buttonMultiply.setText("*");
-        buttonMultiply.setSize(40, 40);    
         buttonDivide.setText("/");
-        buttonDivide.setSize(40, 40);    
-        buttonSinus.setText("sin");
-        buttonSinus.setSize(40, 40);    
+        buttonSinus.setText("sin");   
         buttonCosinus.setText("cos");
-        buttonCosinus.setSize(40, 40);    
         buttonTangens.setText("tan");
-        buttonTangens.setSize(40, 40);    
         buttonInvert.setText("+-");
-        buttonInvert.setSize(40, 40);    
-        buttonPoint.setText(".");
-        buttonPoint.setSize(40, 40);    
+        buttonPoint.setText(".");  
         buttonBackspace.setText("<-");
-        buttonBackspace.setSize(80, 40);    
         buttonClearAll.setText("Clear All");
-        buttonClearAll.setSize(40, 40);    
-        buttonEquals.setText("=");
-        buttonEquals.setSize(40, 80);        
+        buttonEquals.setText("="); 
+        buttonParenthesisOpen.setText("(");
+        buttonParenthesisClosed.setText(")");
     }
     
     public void ResetEingabe()
