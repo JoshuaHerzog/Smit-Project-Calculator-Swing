79f0a412bf47304ef43911fc6e76b9d905d34749
Jan Groß <minzkraut@gmail.com>
2016-12-19 21:22:31 +0100

    Kommentare überarbeitet

--- Taschenrechner/src/Model/Model.java
+++ Taschenrechner/src/Model/Model.java
@@ -1,114 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 import java.util.*;
+
 /**
- *
- * 
- * @author Jan M. Groß, Joshua Herzog, Hendrik Neumannm
- * The Data Layer.
+ * @author Jan M. Groß, Hendrik Neumannm.
  */
-public class Model 
-{
+public class Model {
+
     public String Berechne(String astr) {
-    String str = astr;
-    //Create anonymus Class with a temporary object instance.
-    return Double.toString(new Object() {
-        int pos = -1, ch;
+        String str = astr;
+        //Erstelle Klasse mit einem temporaerem Objekt
+        return Double.toString(new Object() {
+            int position = -1, zeichen;
 
-        /*on the first Call it set the char pointer to the end of the calcstring 
-        and then the other calls it count down to the begin of the string*/
-        void nextChar() {
-            ch = (++pos < str.length()) ? str.charAt(pos) : -1;
-        }
-
-        //This function has appetite
-        //Nom Nom Nom
-        boolean eat(int charToEat) {
-            while (ch == ' ') nextChar();
-            if (ch == charToEat) {
-                nextChar();
-                return true;
-            }
-            return false;
-        }
-
-        //Parse the Calculation and return the result
-        double parse() {
-            nextChar();
-            double x = parseExpression();
-            if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
-            return x;
-        }
-
-        // Grammar:
-        // expression = term | expression `+` term | expression `-` term
-        // term = factor | term `*` factor | term `/` factor
-        // factor = `+` factor | `-` factor | `(` expression `)`
-        //        | number | functionName factor | factor `^` factor
-
-        //calculate the expressions with +and-
-        double parseExpression() {
-            //Before do that calculate *and/
-            double x = parseTerm();
-            for (;;) {
-                if      (eat('+')) x += parseTerm(); // addition
-                else if (eat('-')) x -= parseTerm(); // subtraction
-                else return x;
-            }
-        }
-
-        //calculate the expressions with *and/
-        double parseTerm() {
-            //Before do that calculate extra expression calc like cos,sin,tan....  
-            double x = parseFactor();
-            for (;;) {
-                if      (eat('*')) x *= parseFactor(); // multiplication
-                else if (eat('/')) x /= parseFactor(); // division
-                else return x;
-            }
-        }
-
-        //calculate the expressions with sin,cos,tan,sqrt,power,()....
-        double parseFactor() {
-            if (eat('+')) return parseFactor(); // unary plus
-            if (eat('-')) return -parseFactor(); // unary minus
-
-            double x;
-            int startPos = this.pos;
-            if (eat('(')) { // parentheses
-                x = parseExpression();
-                eat(')');
-            } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-                while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-                x = Double.parseDouble(str.substring(startPos, this.pos));
-            } else if (ch >= 'a' && ch <= 'z') { // functions
-                while (ch >= 'a' && ch <= 'z') nextChar();
-                String func = str.substring(startPos, this.pos);
-                x = parseFactor();
-                if (func.equals("sqrt")) x = Math.sqrt(x);
-                else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                else throw new RuntimeException("Unbekannte Funktion: " + func);
-            } else {
-                throw new RuntimeException("Unerwartet: " + (char)ch);
+            /*Beim ersten Aufruf wird der Pointer auf das Ende 
+            des zu berechnenden Strings gesetzt und runtergezaehlt*/
+            void naechstes_zeichen() {
+                zeichen = (++position < str.length()) ? str.charAt(position) : -1;
             }
 
-            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+            // Vergleiche das erwartete Zeichen mit dem aus dem String an der gegebenen Position
+            boolean vergleichen(int vergleich_zeichen) {
+                while (zeichen == ' ') {
+                    naechstes_zeichen();
+                }
+                if (zeichen == vergleich_zeichen) {
+                    naechstes_zeichen();
+                    return true;
+                }
+                return false;
+            }
 
-            return x;
-        }
-    }.parse()); //Call Parse to Calculate the string.
-}
-   
-   //Nothing to see here.
-   public void ClearAll()
-   {
-   //empty    
-   }
+            //Parse den Rechenstring und loese komplexe Berechnungen zuerst
+            double parse() {
+                naechstes_zeichen();
+                double ergebnis = parse_ausdruck();
+                if (position < str.length()) {
+                    throw new RuntimeException("Unerwartetes: " + (char) zeichen);
+                }
+                return ergebnis;
+            }
+
+            //Berechne Plus und MInus
+            double parse_ausdruck() {
+                
+                double ergebnis = parse_term();
+                for (;;) {
+                    if (vergleichen('+')) {
+                        ergebnis += parse_term(); // Addition
+                    } else if (vergleichen('-')) {
+                        ergebnis -= parse_term(); // Substraktion
+                    } else {
+                        return ergebnis;
+                    }
+                }
+            }
+
+            //Berechne Mal und Geteilt
+            double parse_term() {
+
+                double ergebnis = parse_faktor();
+                for (;;) {
+                    if (vergleichen('*')) {
+                        ergebnis *= parse_faktor(); // Multiplikation
+                    } else if (vergleichen('/')) {
+                        ergebnis /= parse_faktor(); // Division
+                    } else {
+                        return ergebnis;
+                    }
+                }
+            }
+
+            //Berechne Winkelfunktionen
+            double parse_faktor() {
+                if (vergleichen('+')) {
+                    return parse_faktor(); // einstelliges Plus
+                }
+                if (vergleichen('-')) {
+                    return -parse_faktor(); // einstelliges Minus
+                }
+                double ergebnis;
+                int anfangs_position = this.position;
+                if (vergleichen('(')) { // Klammern
+                    ergebnis = parse_ausdruck();
+                    vergleichen(')');
+                } else if ((zeichen >= '0' && zeichen <= '9') || zeichen == '.') {
+                    while ((zeichen >= '0' && zeichen <= '9') || zeichen == '.') {
+                        naechstes_zeichen();
+                    }
+                    ergebnis = Double.parseDouble(str.substring(anfangs_position, this.position));
+                } else if (zeichen >= 'a' && zeichen <= 'z') { // Buchstaben bedeutetet Winkelfunktion
+                    while (zeichen >= 'a' && zeichen <= 'z') {
+                        naechstes_zeichen();
+                    }
+                    String func = str.substring(anfangs_position, this.position);
+                    ergebnis = parse_faktor();
+                    if (func.equals("sin")) {
+                        ergebnis = Math.sin(Math.toRadians(ergebnis)); // wandle ergebnis er ist Gradzahl um 
+                    } else if (func.equals("cos")) {
+                        ergebnis = Math.cos(Math.toRadians(ergebnis));
+                    } else if (func.equals("tan")) {
+                        ergebnis = Math.tan(Math.toRadians(ergebnis));
+                    } else {
+                        throw new RuntimeException("Unbekannte Funktion: " + func);
+                    }
+                } else {
+                    throw new RuntimeException("Unerwartetes: " + (char) zeichen);
+                }
+
+                return ergebnis;
+            }
+        }.parse()); //Rufe den Parser auf um den Rechenstring zu berechnen
+    }
+
 }
